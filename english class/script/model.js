d430233d41e862080b23c9f9a97bc253eff13588
Everton543 <64697974+Everton543@users.noreply.github.com>
2021-11-30 10:31:33 -0300

    Add files via upload

---
+++ english class/script/model.js
@@ -0,0 +1,146 @@
+
+var horaEscolhida = 0;
+var diaEscolhido = 0;
+var semanaEscolhida = 0;
+var data = new Date();
+var dia = data.getDate();
+var diaSemana = data.getDay() + 1; //0 = domingo por isso a soma
+var mes = data.getMonth(); //0 = janeiro
+var ano = data.getFullYear();
+var primeiroDiaSemanaMes = diaSemana;
+var historico = [];
+var diaAtual = [diaSemana, dia];
+var diaOcupado = {};
+var quantidadeDiasOcupados = 0;
+var studentsList ={};
+var currentStudent = new Student("Beatriz", "ckmfNnhYEkSPTxG3K8UbdhN4Aci1", true);
+
+function hasClass(element, className) {
+  return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
+}
+
+var userCurrent = firebase.auth().currentUser;
+var storage = firebase.storage();
+
+/*function saveAudioFromFile(){
+    var files = document.getElementById("upload").files[0];
+    document.getElementById("audio").load();
+    var storageRef = firebase.storage().ref().child('audio/' + files.name);
+    storageRef.put(files);
+}*/
+
+function userNotLogged(){
+    console.log("User not logged");
+}
+var storage = firebase.storage();
+
+function colocarDiaOcupado(id){
+  let reference = "Students/" + id + '/' + ano + '/' + mes;
+  firebase.database().ref(reference).once('value', (snapshot) => {
+    snapshot.forEach(function (childSnapshot) {
+      diaOcupado[childSnapshot.val().dia] = 1;
+      quantidadeDiasOcupados += 1;
+    });
+    criarCalendario(id);
+});
+}
+
+function createStudentList(){
+  let list = "";
+  for( key in studentsList){
+    list += "<button class='student ";
+    if(studentsList[key].newAudio != null && studentsList[key].newAudio){
+      list += "newAudio";
+    }
+    
+    list += " ' onclick='createStudentCalendar(\"" + studentsList[key].id + "\")'>" + studentsList[key].name + "</button>";
+  }
+
+  document.getElementById("studentList").innerHTML = list;
+}
+
+function createStudentCalendar(id){
+  document.getElementById("calendario").classList.remove("hide");
+  diaOcupado = {};
+  quantidadeDiasOcupados = 0;
+  currentStudent = studentsList[id];
+  console.log(currentStudent);
+  apagarCalendario();
+  colocarDiaOcupado(id);
+}
+
+function getStudentsInfo(){
+  let reference = "Students/";
+  firebase.database().ref(reference).once('value', (snapshot) => {
+    snapshot.forEach(function (childSnapshot) {
+      let info = childSnapshot.val().info;
+      studentsList[info.id] = new Student(info.name, info.id, info.newAudio);
+      /*todo: 
+      / 2- Fazer com que a função dia click se comporte diferente
+      /    2.1 - para que eu mande o audio de acordo com o aluno
+      /    2.2 -  ao mandar o audio marcar que mandei a resposta
+      / 
+      */
+    });
+    console.log(studentsList);
+    createStudentList();
+  });
+}
+
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      if(user.uid == "vOdPdTtvKah6PoMS8ymFQQuO0iw2"){
+        document.getElementById("calendario").classList.add("hide");
+          getStudentsInfo();
+      }else{
+      userCurrent = user;
+      colocarDiaOcupado(user.uid);
+      }
+    }
+    else {
+        userNotLogged();
+    }
+})
+
+function checarResposta(d){
+  aparecerRecorder();
+  let reference = "";
+  if(userCurrent != null){
+    document.getElementById("recordButton").classList.add("hide");
+    reference = "Students/" + userCurrent.uid + '/' + ano + '/' + mes + '/' + d;
+  }else{
+    reference = "Students/" + currentStudent.id + '/' + ano + '/' + mes + '/' + d;
+  }
+  //let reference = "audio/Sun, 28 Nov 2021 23:24:05 GMT";
+  console.log(reference);
+  firebase.storage().ref(reference).getDownloadURL().then(function(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'blob';
+    xhr.onload = function(event) {
+      var blob = xhr.response;
+    };
+  
+    var audio = document.getElementById('savedAudio');
+    audio.src = url;
+  }).catch(function(error) {
+    // Handle any errors
+  });
+}
+
+function logout(){
+  firebase.auth().signOut().then(function() {
+    window.location = 'index.html';
+  }, function(error) {
+    // An error happened.
+  });
+}
+
+function salvarNomeUsuario(){
+  if(userCurrent != null){
+  let reference = "Students/" + userCurrent.uid + '/info' ;
+  firebase.database().ref(reference).set({
+    "name" : userCurrent.displayName,
+    "id" : userCurrent.uid
+  });
+}
+}
---
+++ english class/script/recorder.js
@@ -0,0 +1,11 @@
+function aparecerRecorder(){
+    document.getElementById('holder').classList.remove('hide');
+    document.getElementById('calendario').classList.add('hide');
+}
+
+function fecharRecorder(){
+    diaEscolhido = null;
+    document.getElementById("recordButton").classList.remove("hide");
+    document.getElementById('holder').classList.add('hide');
+    document.getElementById('calendario').classList.remove('hide'); 
+}
